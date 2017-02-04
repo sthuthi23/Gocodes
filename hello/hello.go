0b41156a6ef2519ae0b2ee75f61d6270bd23d3ed
Sthuthi <sbhat@MacBook-Air.local>
2017-02-03 16:09:46 -0800

    adding reverse package.

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+//To build and install this : go install github.com/user/hello
+package main
+
+import (
+	"fmt"
+
+	"github.com/user/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!!!oG oT emocleW")
+}
--- hello_feb3/hello.go
+++
@@ -1,8 +0,0 @@
-//To build and install this : go install github.com/user/hello
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Printf("hello, Sthuthi\n")
-}
